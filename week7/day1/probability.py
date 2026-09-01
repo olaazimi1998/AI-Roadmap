b498508b9d7fd9f7d00b606ad25e1f877dbc9b02
olaazimi1998 <olaazimi1998@gmail.com>
2026-09-01 20:25:12 +0400

    week 8 added

---
+++ week7/day1/probability.py
@@ -0,0 +1,51 @@
+#Probability — احتمال
+#
+#Probability
+#
+#Conditional Probability
+#
+#Bayes' Theorem
+#
+#Distributions
+#
+#Calculus — حساب دیفرانسیل
+#
+#Derivative
+#
+#Gradient
+#
+#Chain Rule
+#
+#ارتباط آن‌ها با آموزش مدل‌های ML
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
